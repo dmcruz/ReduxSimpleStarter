7da963e1e3d6185c8a54dfad3987abdab2001e55
Donna <dmcruz@gmail.com>
2017-03-13 21:31:53 +0800

    redux

---
+++ src/containers/book-list.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        { this.renderList() }
+      </ul>
+    )
+  }
+
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className='list-group-item'>{book.title}</li>
+      );
+    });
+  }
+}
+
+function mapStateToProps(state) {
+  // whatever is returned will now show up inside of BookList
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
