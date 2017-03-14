73e54de64f84bec04a8a00df17d191d730c32b91
Donna <dmcruz@gmail.com>
2017-03-14 22:51:45 +0800

    reducers

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 class BookList extends Component {
   render() {
     return (
@@ -13,7 +14,12 @@ class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className='list-group-item'>{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book) }
+          className='list-group-item'>
+          {book.title}
+        </li>
       );
     });
   }
@@ -26,4 +32,8 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
